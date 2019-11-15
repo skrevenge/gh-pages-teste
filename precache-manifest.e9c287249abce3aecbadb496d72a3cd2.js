3aa0b1c69d419ca6dd9b2492835726f0f4bcafc9
skrevenge <edu-pontesw@hotmail.com>
2019-11-15 17:51:57 -0300

    Updates

---
+++ precache-manifest.e9c287249abce3aecbadb496d72a3cd2.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b3f357c3dd131293dcc2b150d054d7",
+    "url": "/gh-pages-teste/index.html"
+  },
+  {
+    "revision": "5d03ee9ecb9a86352c62",
+    "url": "/gh-pages-teste/static/css/main.12c6b5ca.chunk.css"
+  },
+  {
+    "revision": "13cb598fd989e16911b9",
+    "url": "/gh-pages-teste/static/js/2.01c0cde2.chunk.js"
+  },
+  {
+    "revision": "5d03ee9ecb9a86352c62",
+    "url": "/gh-pages-teste/static/js/main.e7a1d3ff.chunk.js"
+  },
+  {
+    "revision": "1965bdd027e681109263",
+    "url": "/gh-pages-teste/static/js/runtime-main.f55cce93.js"
+  },
+  {
+    "revision": "73c5ff8f80f110bce3a382fc1cc28072",
+    "url": "/gh-pages-teste/static/media/EX1.73c5ff8f.png"
+  },
+  {
+    "revision": "1ed3a4569d60f2a8f37cebb9d4f3ed44",
+    "url": "/gh-pages-teste/static/media/byJonhS.1ed3a456.png"
+  },
+  {
+    "revision": "705fdaf26b7188a5fc4cc0b3b3fbbd27",
+    "url": "/gh-pages-teste/static/media/dudu.705fdaf2.png"
+  },
+  {
+    "revision": "f2d1487e6b8a7aca0b9e9864bc362a51",
+    "url": "/gh-pages-teste/static/media/gcs1.f2d1487e.png"
+  },
+  {
+    "revision": "843c3b5d692b8efc448d82848628333a",
+    "url": "/gh-pages-teste/static/media/gcs2.843c3b5d.png"
+  },
+  {
+    "revision": "c25663197f4ec097cc766f9bb6a2e858",
+    "url": "/gh-pages-teste/static/media/js1.c2566319.png"
+  },
+  {
+    "revision": "492f312b9556e8daf4869ecdd5028e9c",
+    "url": "/gh-pages-teste/static/media/sdgg3d 2.492f312b.png"
+  },
+  {
+    "revision": "6c6c366483a61a9e6a154245ed36eab8",
+    "url": "/gh-pages-teste/static/media/sdgg3d 3.6c6c3664.png"
+  },
+  {
+    "revision": "16058acc577c26b22845e37c1694cc73",
+    "url": "/gh-pages-teste/static/media/sdgg3d 4.16058acc.png"
+  },
+  {
+    "revision": "5311468593cdc02b0918317e091186e5",
+    "url": "/gh-pages-teste/static/media/sdgg3d.53114685.png"
+  },
+  {
+    "revision": "f78a730652a19a73c85bd47eb9ac265c",
+    "url": "/gh-pages-teste/static/media/sp11-1.f78a7306.png"
+  },
+  {
+    "revision": "7399b4735de69873443423791bfe349f",
+    "url": "/gh-pages-teste/static/media/sp11-2.7399b473.png"
+  },
+  {
+    "revision": "c1a29b0aa8b44e3860fe4d3460f3d2e5",
+    "url": "/gh-pages-teste/static/media/sp11-3.c1a29b0a.png"
+  },
+  {
+    "revision": "7cf7d1d15953b7bc5cdf5591c2e73264",
+    "url": "/gh-pages-teste/static/media/sp11-4.7cf7d1d1.png"
+  },
+  {
+    "revision": "f5aacc544644ee2041f19d8c2432624a",
+    "url": "/gh-pages-teste/static/media/st.f5aacc54.png"
+  },
+  {
+    "revision": "64c92ff4d26a7c49a1fa7ef6fc543c95",
+    "url": "/gh-pages-teste/static/media/ud1.64c92ff4.png"
+  },
+  {
+    "revision": "027e7f929c044bbae82cce34102eb91e",
+    "url": "/gh-pages-teste/static/media/ud2.027e7f92.png"
+  },
+  {
+    "revision": "30d7dbb0634c1b90a69f5f0779417eac",
+    "url": "/gh-pages-teste/static/media/wp.30d7dbb0.png"
+  },
+  {
+    "revision": "7d26b077310a18ee1e00d81cd23a89ed",
+    "url": "/gh-pages-teste/static/media/xf1.7d26b077.png"
+  },
+  {
+    "revision": "4dcb0ae7bf36edeb3df2acd41b1530f8",
+    "url": "/gh-pages-teste/static/media/xf2.4dcb0ae7.png"
+  },
+  {
+    "revision": "82de1bbba952aff77e4c49f41de3469a",
+    "url": "/gh-pages-teste/static/media/xf3.82de1bbb.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.62b72a2526cf54abf43225b05af61f0e.js"
+  "/gh-pages-teste/precache-manifest.e9c287249abce3aecbadb496d72a3cd2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gh-pages-teste/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
